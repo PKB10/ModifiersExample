cb37e3e07a2415f9512b3f220ce004872974e09a
Priyanka K Bhatia <priyanka.k.bhatia@gmail.com>
2024-02-21 18:35:06 +0530

    Initial Commit

---
+++ ModifiersExamples/ModifiersExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModifiersExamplesApp.swift
+//  ModifiersExamples
+//
+//  Created by Priyanka on 21/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ModifiersExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
